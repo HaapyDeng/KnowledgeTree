ce07076e633c90f02464bdc7ccc78666c9ee92b0
happyzz <jzhou_deng@163.com>
2017-10-09 15:04:57 +0800

    框架搭建和闪屏建立

--- app/src/main/java/com/max_plus/knowledgetree/activity/LoginActivity.java
+++ app/src/main/java/com/max_plus/knowledgetree/activity/LoginActivity.java
@@ -1,13 +1,15 @@
-package com.max_plus.knowledgetree;
+package com.max_plus.knowledgetree.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+import com.max_plus.knowledgetree.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_login);
     }
 }
