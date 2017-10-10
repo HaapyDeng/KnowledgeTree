4e13f452ce0db59a5e2a9c74e2d0a1d42c3be474
happyzz <jzhou_deng@163.com>
2017-10-10 14:31:13 +0800

    重设密码和注册UI

---
+++ app/src/main/java/com/max_plus/knowledgetree/activity/ResetPasswordActivity.java
@@ -0,0 +1,15 @@
+package com.max_plus.knowledgetree.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.max_plus.knowledgetree.R;
+
+public class ResetPasswordActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
