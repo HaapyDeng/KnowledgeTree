3185854aae5ed829b62ea885fbdea264b87b6336
happyzz <jzhou_deng@163.com>
2017-10-17 17:32:12 +0800

    我的页面布局

--- app/src/main/java/com/max_plus/knowledgetree/activity/MyFragment.java
+++ app/src/main/java/com/max_plus/knowledgetree/activity/MyFragment.java
@@ -8,16 +8,8 @@ import android.view.ViewGroup;
 
 import com.max_plus.knowledgetree.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MyFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MyFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MyFragment extends Fragment {
-
+    private View mRootView;
 
     // TODO: Rename and change types and number of parameters
     public static MyFragment newInstance() {
@@ -28,12 +20,17 @@ public class MyFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initView();
+    }
+
+    private void initView() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_my, container, false);
+        mRootView = inflater.inflate(R.layout.fragment_my, container, false);
+        return mRootView;
     }
 
 }
