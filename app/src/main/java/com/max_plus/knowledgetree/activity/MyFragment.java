b248532c06f936631507d004f71e9e64d64ff616
happyzz <jzhou_deng@163.com>
2017-10-17 18:54:46 +0800

    照相从底部弹出popowindow

--- app/src/main/java/com/max_plus/knowledgetree/activity/MyFragment.java
+++ app/src/main/java/com/max_plus/knowledgetree/activity/MyFragment.java
@@ -1,15 +1,25 @@
 package com.max_plus.knowledgetree.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
 
 import com.max_plus.knowledgetree.R;
+import com.max_plus.knowledgetree.view.TakePhotoPopWin;
 
-public class MyFragment extends Fragment {
+public class MyFragment extends Fragment implements View.OnClickListener {
     private View mRootView;
+    private ImageView takePhoto;
+    private Context mContext;
+    private WindowManager.LayoutParams params;
+
 
     // TODO: Rename and change types and number of parameters
     public static MyFragment newInstance() {
@@ -20,17 +30,52 @@ public class MyFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = this.getActivity();
+        LayoutInflater layoutInflater = getActivity().getLayoutInflater();
+        mRootView = layoutInflater.inflate(R.layout.fragment_my,
+                (ViewGroup) getActivity().findViewById(R.id.fragment_container), false);
         initView();
     }
 
     private void initView() {
+        takePhoto = mRootView.findViewById(R.id.iv_takePhoto);
+        takePhoto.setOnClickListener(this);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         mRootView = inflater.inflate(R.layout.fragment_my, container, false);
+        initView();
         return mRootView;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.iv_takePhoto:
+                showPopFormBottom(mRootView);
+        }
+    }
+
+    private void showPopFormBottom(View view) {
+
+        TakePhotoPopWin takePhotoPopWin = new TakePhotoPopWin(getActivity());
+        //        设置Popupwindow显示位置（从底部弹出）
+        takePhotoPopWin.showAtLocation(view, Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
+        params = getActivity().getWindow().getAttributes();
+        //当弹出Popupwindow时，背景变半透明
+        params.alpha = 0.7f;
+        getActivity().getWindow().setAttributes(params);
+        //设置Popupwindow关闭监听，当Popupwindow关闭，背景恢复1f
+        takePhotoPopWin.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                params = getActivity().getWindow().getAttributes();
+                params.alpha = 1f;
+                getActivity().getWindow().setAttributes(params);
+            }
+        });
+
+    }
 }
---
+++ app/src/main/java/com/max_plus/knowledgetree/view/TakePhotoPopWin.java
@@ -0,0 +1,87 @@
+package com.max_plus.knowledgetree.view;
+
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import com.max_plus.knowledgetree.R;
+
+/**
+ * Created by djz on 2017/10/17.
+ */
+
+public class TakePhotoPopWin extends PopupWindow implements View.OnClickListener {
+    private Context mContext;
+
+    private View view;
+
+    private TextView take_phone, choose_album, choose_system;
+
+    public TakePhotoPopWin(Context mContext) {
+        this.view = LayoutInflater.from(mContext).inflate(R.layout.take_photo_pop, null);
+        take_phone = view.findViewById(R.id.take_phone);
+        take_phone.setOnClickListener(this);
+
+        choose_album = view.findViewById(R.id.choose_album);
+        choose_album.setOnClickListener(this);
+
+        choose_system = view.findViewById(R.id.choose_system);
+        choose_system.setOnClickListener(this);
+
+        this.setOutsideTouchable(true); // 设置外部可点击
+
+
+    /* 设置弹出窗口特征 */
+        // 设置视图
+        this.setContentView(this.view);
+        // 设置弹出窗体的宽和高
+        this.setHeight(LinearLayout.LayoutParams.WRAP_CONTENT);
+        this.setWidth(LinearLayout.LayoutParams.MATCH_PARENT);
+
+        // 设置弹出窗体可点击
+        this.setFocusable(true);
+
+        // 实例化一个ColorDrawable颜色为半透明
+        ColorDrawable dw = new ColorDrawable(0xb0000000);
+        // 设置弹出窗体的背景
+        this.setBackgroundDrawable(dw);
+
+        // 设置弹出窗体显示时的动画，从底部向上弹出
+        this.setAnimationStyle(R.style.take_photo_anim);
+
+
+        // mMenuView添加OnTouchListener监听判断获取触屏位置如果在选择框外面则销毁弹出框
+        this.view.setOnTouchListener(new View.OnTouchListener()
+
+        {
+            public boolean onTouch(View v, MotionEvent event) {
+                int height = view.findViewById(R.id.pop_layout).getTop();
+                int y = (int) event.getY();
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (y < height) {
+                        dismiss();
+                    }
+                }
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.take_phone:
+                break;
+            case R.id.choose_album:
+                break;
+            case R.id.choose_system:
+                break;
+        }
+    }
+}
